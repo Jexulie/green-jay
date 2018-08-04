57a9cdbdb304bf28253dd330b69069e21c266a46
Jexulie <fejitj3n@yahoo.com>
2018-08-04 08:41:21 +0300

    rgb fixed

--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,7 @@ greenjay.createLogger({
     useFile: true,
     modifiers: {
         date:{
-            color: '#069'
+            color: '(152,185,52)'
         },
         message: {
             modify: 'underline'
@@ -16,37 +16,37 @@ greenjay.createLogger({
             filePath: './log/warns.log',
             minLevel: 'warning'
         }),
-        new greenjay.logger({
-            filePath: './log/trivial.log',
-            minLevel: 'trivial'
-        }),
-        new greenjay.logger({
-            filePath: './log/info.log',
-            minLevel: 'info'
-        }),
-        new greenjay.logger({
-            filePath: './log/error.log',
-            minLevel: 'error'
-        }),
-        new greenjay.logger({
-            filePath: './log/emergency.log',
-            minLevel: 'emergency'
-        }),
-        new greenjay.logger({
-            filePath: './log/alert.log',
-            minLevel: 'alert'
-        })
+        // new greenjay.logger({
+        //     filePath: './log/trivial.log',
+        //     minLevel: 'trivial'
+        // }),
+        // new greenjay.logger({
+        //     filePath: './log/info.log',
+        //     minLevel: 'info'
+        // }),
+        // new greenjay.logger({
+        //     filePath: './log/error.log',
+        //     minLevel: 'error'
+        // }),
+        // new greenjay.logger({
+        //     filePath: './log/emergency.log',
+        //     minLevel: 'emergency'
+        // }),
+        // new greenjay.logger({
+        //     filePath: './log/alert.log',
+        //     minLevel: 'alert'
+        // })
     ]
 });
 
 
 greenjay.emergency('Some Emergency');
-greenjay.alert('Some Alert');
-greenjay.critical('Some Critical');
-greenjay.error('Some Error');
-greenjay.warning('Some Warning');
-greenjay.info('Some Info');
-greenjay.debug('Some Debug');
-greenjay.trivial('Some Trivial');
+// greenjay.alert('Some Alert');
+// greenjay.critical('Some Critical');
+// greenjay.error('Some Error');
+// greenjay.warning('Some Warning');
+// greenjay.info('Some Info');
+// greenjay.debug('Some Debug');
+// greenjay.trivial('Some Trivial');
 
  
